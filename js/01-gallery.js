516cc3824ccd4af22f48c677f144c753300f544b
Marius Birsan <marius.birsan90@outlook.com>
2023-10-29 10:29:14 +0200

    Update first exercise

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,14 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery');
+
+galleryItems.forEach((image) => {
+  const li = document.createElement("li");
+  galleryList.append(li);
+  li.classList.add("gallery__item");
+  const imgElement = `<img class="gallery__image" src=${image.preview} alt=${image.description} />`;
+  li.insertAdjacentHTML("afterbegin", imgElement);
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+galleryItems.forEach((image) => {
+  const li = document.createElement("li");
+  galleryList.append(li);
+  li.classList.add("gallery__item");
+  const imgElement = `<img class="gallery__image" src=${image.preview} data-source=${image.original} alt=${image.description} />`;
+  li.insertAdjacentHTML("afterbegin", imgElement);
+});
+
+const imageList = document.querySelectorAll("img");
+
+imageList.forEach((image) => {
+  image.addEventListener("click", (e) => {
+    galleryItems.forEach((item) => {
+      if (item.description.includes(`${image.alt}`)) {
+        basicLightbox
+          .create(
+            `
+            <img src=${image.dataset.source}>
+    `
+          )
+          .show();
+      }
+    });
+  });
+});
+
+
